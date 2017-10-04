473a52af73352b57a7fc52f2fe1a3ca29ea00280
Simone Zocchi <zsimone93@gmail.com>
2017-10-04 19:03:51 +0200

    fix startup

--- startup.sh
+++ startup.sh
@@ -18,11 +18,11 @@ docker run --name mongoDB --network host -d zsimo/rpi-mongo --replSet foo
 if [ "$TH_ROLE" = "MASTER" ]; then
     echo "Enter local IP [ENTER]:"
     read ip
-    python main.py $ip
+    python ./app/main.py $ip
 else
     echo "Add the node to the system and when done press [ENTER]"
     read enter
-    python main.py
+    python ./app/main.py
 fi
 
 docker stop mongoDB
