d81d012a0a75f576baa94ac651543a126ac920a6
Simone Zocchi <zsimone93@gmail.com>
2017-10-05 11:02:13 +0200

    fix

--- core/APIGateway/__init__.py
+++ core/APIGateway/__init__.py
@@ -1,4 +1,5 @@
 import actions, aws, invoker, nodes, sequences, userfile, validator, internalGW
+from mainAPI import run
 from flask import Flask, request, make_response, jsonify
 
 __all__ = ["run"]
