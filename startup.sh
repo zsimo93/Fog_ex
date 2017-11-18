638be36bf242efc061c294de53ad85ba027cf0df
Simone Zocchi <zsimone93@gmail.com>
2017-11-18 16:11:20 +0100

    fix

--- startup.sh
+++ startup.sh
@@ -7,12 +7,14 @@ then
     #docker pull zsimo/python-image:imageProc
     #docker pull zsimo/python-image:base
     #docker pull zsimo/python-image:ffmeg
+    docker run --name mongoDB --network host -d zsimo/rpi-mongo --replSet foo
     arch="arm";
 else
     echo "x86 detected"
     #docker pull zsimo/python-image:imageProc_x86
     #docker pull zsimo/python-image:base_x86
     #docker pull zsimo/python-image:ffmeg_x86
+    docker run --name mongoDB --network host -d mongo:3.0.15 --replSet foo
     arch="x86";
 fi
 export TH_ARCH=$arch
@@ -20,8 +22,6 @@ export TH_ARCH=$arch
 echo "Enter role [ENTER]:"
 read role
 export TH_ROLE=$role
-
-docker run --name mongoDB --network host -d zsimo/rpi-mongo --replSet foo
 
 echo "Enter MASTER IP [ENTER]:"
 read ip
